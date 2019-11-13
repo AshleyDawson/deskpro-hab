f0096b2982a5d193fbdbf68430f6042dfe89e3c1
Ashley Dawson <ashley.dawson@deskpro.com>
2019-11-13 14:23:37 +0000

    Fixed NPM/NodeJS versions

--- src/Resources/boilerplate/provision/scripts/configure-node.sh
+++ src/Resources/boilerplate/provision/scripts/configure-node.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-echo "export PATH=/usr/local/lib/nodejs/node-v8.11.4-linux-x64/bin:\$PATH" >> /home/vagrant/.profile
-ln -s /usr/local/lib/nodejs/node-v8.11.4-linux-x64/bin/node /usr/bin/node
-ln -s /usr/local/lib/nodejs/node-v8.11.4-linux-x64/bin/npm /usr/bin/npm
+echo "export PATH=/usr/local/lib/nodejs/node-v8.11.1-linux-x64/bin:\$PATH" >> /home/vagrant/.profile
+ln -s /usr/local/lib/nodejs/node-v8.11.1-linux-x64/bin/node /usr/bin/node
+ln -s /usr/local/lib/nodejs/node-v8.11.1-linux-x64/bin/npm /usr/bin/npm
 
-npm cache clean --force
+npm install npm@5.7.1 -g
 
 npm install -g \
     bower \
     gulp \
     cpx
 
-npm cache clean --force
+chown -R vagrant:vagrant /home/vagrant/.npm
+chown -R vagrant:vagrant /home/vagrant/.config
--- src/Resources/boilerplate/provision/scripts/install-node.sh
+++ src/Resources/boilerplate/provision/scripts/install-node.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-wget https://nodejs.org/download/release/v8.11.4/node-v8.11.4-linux-x64.tar.xz
 mkdir -p /usr/local/lib/nodejs
-tar xf node-v8.11.4-linux-x64.tar.xz -C /usr/local/lib/nodejs
+
+wget https://nodejs.org/download/release/v8.11.1/node-v8.11.1-linux-x64.tar.xz
+tar xf node-v8.11.1-linux-x64.tar.xz -C /usr/local/lib/nodejs
